395a26b545e6784486edd3550736c261416f4168
-_- <motamota6937@gmail.com>
2025-11-12 17:23:16 +0300

    ...

--- Manufacture_DE/App.xaml.cs
+++ Manufacture_DE/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Manufacture_DE.Model;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,6 @@ namespace Manufacture_DE
     /// </summary>
     public partial class App : Application
     {
+        public static Entities context = new Entities();
     }
 }
